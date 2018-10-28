61dbcc621c9d781e1341e6619a7204a2fce0c9e3
Steve Mustanski <steve.mustanski@gmail.com>
2018-10-28 16:11:08 -0400

    added tests for multiple ships and fixed bug in checkForShips

--- game_logic/ship_methods.js
+++ game_logic/ship_methods.js
@@ -1,16 +1,17 @@
-function checkForShip (player, coordinates) {
+function checkForShip(player, coordinates) {
   let shipPresent, ship;
 
   for (let i = 0; i < player.ships.length; i++) {
     ship = player.ships[i];
     shipPresent = ship.locations.filter((actualCoordinate) => {
-      return (actualCoordinate[0] === coordinates[0]) && (actualCoordinate[1] === coordinates[1]); 
+      return (actualCoordinate[0] === coordinates[0]) && (actualCoordinate[1] === coordinates[1]);
     })[0];
 
-      if (!shipPresent) {
-        return false;
-      }
+    if (shipPresent) {
+      return true;
+    }
   }
+  return false;
 }
 
 module.exports.checkForShip = checkForShip;
--- test/ship_test.js
+++ test/ship_test.js
@@ -13,4 +13,43 @@ describe('check for ship', () => {
     }
     expect(checkForShip(player, [9, 9])).to.be.false;
   });
+  it('should correctly report a ship at a given players coordinate', () => {
+    player = {
+      ships: [
+        {
+          locations: [[0,0]]
+        }
+      ]
+    }
+    expect(checkForShip(player, [0, 0])).to.be.true;
+  });
+  it('should handle ships located at more than one coordinate', () => {
+    player = {
+      ships: [
+        {
+          locations: [[0,0], [0,1]]
+        }
+      ]
+    }
+    expect(checkForShip(player, [0, 1])).to.be.true;
+    expect(checkForShip(player, [0, 0])).to.be.true;
+    expect(checkForShip(player, [9, 9])).to.be.false;
+  });
+  it('should handle ships located at more than one location', () => {
+    player = {
+      ships: [
+        {
+          locations: [[0,0], [0,1]]
+        },
+        {
+          locations: [[1,0], [1,1]]
+        }
+      ]
+    }
+    expect(checkForShip(player, [0, 1])).to.be.true;
+    expect(checkForShip(player, [0, 0])).to.be.true;
+    expect(checkForShip(player, [1, 0])).to.be.true;
+    expect(checkForShip(player, [1, 1])).to.be.true;
+    expect(checkForShip(player, [9, 9])).to.be.false;
+  });
 });
