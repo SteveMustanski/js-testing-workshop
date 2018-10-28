4f5c6baae5de87b466fba01941aeb363e6ff2d6c
Steve Mustanski <steve.mustanski@gmail.com>
2018-10-28 14:21:51 -0400

    set up mocha, sanity check, and test structure

---
+++ test/main_test.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect;
+
+// test suite
+describe('Mocha', () => {
+  // Test spec (unit test)
+  it('should run our tests ung npm', () => {
+    expect(true).to.be.ok;
+  });
+});
