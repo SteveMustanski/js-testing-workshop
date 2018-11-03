91ecc51298ea0b4e180d58a51008fa25a0aadc30
Steve Mustanski <steve.mustanski@gmail.com>
2018-11-03 10:12:20 -0400

    some comments didn't get saved in last commit ???

--- test/game_test.js
+++ test/game_test.js
@@ -2,6 +2,8 @@ const expect = require('chai').expect;
 
 describe('GAME INSTANCE FUNCTIONS', function (){
   describe('checkGameSatus', function () {
+    // pending tests do not have callback
+    // or would have an x infront like 'xdescribe' or 'xit'
     it('should tell me when the game is over');
   })
 })
--- test/main_test.js
+++ test/main_test.js
@@ -1,9 +1,10 @@
 const expect = require('chai').expect;
 
 // test suite
+// sanity check, is mocha and chai set up
 describe('Mocha', () => {
   // Test spec (unit test)
-  it('should run our tests ung npm', () => {
+  it('should run our tests using npm', () => {
     expect(true).to.be.ok;
   });
 });
