f80ccab39237efc58565681f396791d10eeb237f
Steve Mustanski <steve.mustanski@gmail.com>
2018-11-02 22:16:21 -0400

    added pending test example

---
+++ test/game_test.js
@@ -0,0 +1,7 @@
+const expect = require('chai').expect;
+
+describe('GAME INSTANCE FUNCTIONS', function (){
+  describe('checkGameSatus', function () {
+    it('should tell me when the game is over');
+  })
+})
